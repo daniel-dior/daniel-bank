0f05f149ca268d0b68dfbb7e4d1758e8dc9ccfbe
daniel-logo <daniel-logo>
2023-11-27 12:31:03 +0100

    js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function openNav() {
-    document.getElementById("myNav").style.height= '60%'
+    document.getElementById("myNav").style.height= '80%'
 }
 function closeNav() {
     document.getElementById('myNav').style.height= '0%'
